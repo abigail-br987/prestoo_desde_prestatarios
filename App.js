c48d83df3ed528edca5525f313255a1bf712f18d
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-03-26 13:12:13 -0500

    add font to files

--- App.js
+++ App.js
@@ -3,14 +3,33 @@ import { StyleSheet, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Provider as PaperProvider, Text } from "react-native-paper";
 import { theme } from "./theme";
+import { Navigation } from "./src/infraestructure/navigation";
+import { useFonts } from "expo-font";
+
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    "SofiaSansSemiCondensed-Regular": require("./assets/fonts/SofiaSansSemiCondensed-Regular.ttf"),
+    "SofiaSansSemiCondensed-Medium": require("./assets/fonts/SofiaSansSemiCondensed-Medium.ttf"),
+    "SofiaSansSemiCondensed-Bold": require("./assets/fonts/SofiaSansSemiCondensed-Bold.ttf"),
+    "SofiaSansSemiCondensed-Black": require("./assets/fonts/SofiaSansSemiCondensed-Black.ttf"), 
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <PaperProvider theme={theme}>
       <View style={styles.container}>
-        <Text variant="regular" style={{ color: theme.colors.primary }}>
-          Welcome to React Native Paper!
+      <Text variant="h1" style={{ color: theme.colors.primary }}>
+      Welcome to React Native Paper!
         </Text>
+
+        <Navigation/>
+        
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
@@ -21,7 +40,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
   },
 });
