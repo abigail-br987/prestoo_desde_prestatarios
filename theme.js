70317d0dcf789be6664b6d1b9a17036b1fa283b4
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-03-26 13:12:52 -0500

    personalize theme based on figma

--- theme.js
+++ theme.js
@@ -4,24 +4,53 @@ export const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: "#6200ea",
-    accent: "#03dac4", 
-    background: "#f5f5f5", 
-    surface: "#ffffff",
-    text: "#333333", 
+    baseColor: "#FEFEF4", 
+    darkerBaseColor: "#C4CEC4",
+    primaryLightColor: "#D4FAF7",
+    primaryAccent:"#00E7A5",
+    secondAccent:"#9435DD",
+    thirdAccent:"#FBCF7C",
+    textColor:"#053654",
+    lightTextColor:"#38677C",
+    intensePrimaryAccent:"#6FF0E5",
   },
   fonts: {
-    regular: {
-      fontFamily: "System",
-      fontWeight: "normal",
+    default: {
+      fontFamily: "SofiaSansSemiCondensed-Regular",
+      fontSize: 16,
+      color: "#053654",
     },
-    medium: {
-      fontFamily: "System",
-      fontWeight: "500",
+    h1: {
+      fontFamily: "SofiaSansSemiCondensed-Black", 
+      fontSize: 64,
+      color: "#053654",
     },
-    bold: {
-      fontFamily: "System",
-      fontWeight: "bold",
+    h2: {
+      fontFamily: "SofiaSansSemiCondensed-Black", 
+      fontSize: 36,
+      letterSpacing: 1.5,
+      color: "#053654",
+    },
+    h3: {
+      fontFamily: "SofiaSansSemiCondensed-ExtraBold", 
+      fontSize: 24,
+      letterSpacing: 2,
+      color: "#053654",
+    },
+    subheading: {
+      fontFamily: "SofiaSansSemiCondensed-BoldItalic", 
+      fontSize: 20,
+      color: "#053654",
+    },
+    label1: {
+      fontFamily: "SofiaSansSemiCondensed-Bold", 
+      fontSize: 16,
+      color: "#053654",
+    },
+    label2: {
+      fontFamily: "SofiaSansSemiCondensed-Italic", 
+      fontSize: 14,
+      color: "#38677C",
     },
   },
 };
