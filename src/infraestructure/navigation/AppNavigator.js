439d8a296bc2167ab27b71419fa22ab86c8f61dd
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-03-26 21:39:54 -0500

    updating new screens

--- src/infraestructure/navigation/AppNavigator.js
+++ src/infraestructure/navigation/AppNavigator.js
@@ -2,10 +2,13 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { View, Text } from "react-native";
-import { MessagesScreen } from "../../features/messages/screens/MessagesScreen";
 import { theme } from "../../../theme";
 import { HomeNavigator } from "./HomeNavigator";
 const Tab = createBottomTabNavigator();
+import { MessageNavigator } from "./MessagesNavigator";
+import MessagesScreen from "../../features/messages/screens/MessagesScreen";
+import PrestamosScreen from "../../features/prestamos/PrestamosScreen";
+
 
 const TAB_ICON = {
   Home: "home",
@@ -21,7 +24,9 @@ const createScreenOptions = ({ route }) => ({
     <Ionicons
       name={TAB_ICON[route.name]}
       size={focused ? 28 : 24}
-      color={focused ? theme.colors.darkerPrimaryAccent : theme.colors.textColor}
+      color={
+        focused ? theme.colors.darkerPrimaryAccent : theme.colors.textColor
+      }
     />
   ),
   tabBarLabelStyle: {
@@ -43,9 +48,9 @@ const createScreenOptions = ({ route }) => ({
 export const AppNavigator = () => (
   <Tab.Navigator screenOptions={createScreenOptions}>
     <Tab.Screen name="Home" component={HomeNavigator} />
-    <Tab.Screen name="Mensajes" component={MessagesScreen} />
-    <Tab.Screen name="Préstamos" component={MessagesScreen} />
-    <Tab.Screen name="Anuncios" component={MessagesScreen} />
-    <Tab.Screen name="Cuenta" component={MessagesScreen} />
+    <Tab.Screen name="Mensajes" component={MessageNavigator} />
+    <Tab.Screen name="Préstamos" component={PrestamosScreen} />
+    <Tab.Screen name="Anuncios" component={HomeNavigator} />
+    <Tab.Screen name="Cuenta" component={HomeNavigator} />
   </Tab.Navigator>
 );
