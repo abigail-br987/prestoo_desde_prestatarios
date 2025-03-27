47d1899c43e86aedef209cf8826c9ee26a01e0a7
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-03-27 02:51:37 -0500

    style

--- src/infraestructure/navigation/MessagesNavigator.js
+++ src/infraestructure/navigation/MessagesNavigator.js
@@ -1,22 +1,53 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import MessagesScreen from "../../features/messages/screens/MessagesScreen"
+import MessagesScreen from "../../features/messages/screens/MessagesScreen";
 import MessagesDetailScreen from "../../features/messages/screens/MessagesDetailScreen";
+import { useTheme } from "react-native-paper";
+import { Ionicons } from "@expo/vector-icons";
+import { Image } from "react-native";
+import { View } from "react-native";
+import { Text } from "react-native";
 
 const MessageStack = createStackNavigator();
 
 export const MessageNavigator = () => {
+  const { colors, fonts } = useTheme();
 
   console.log("MessagesScreen:", MessagesScreen);
 
-
   return (
-    <MessageStack.Navigator screenOptions={{ headerShown: true }}>
+    <MessageStack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: colors.baseColor,
+          elevation: 10,
+        },
+        headerBackTitleVisible: false,
+        headerTitleAlign: "left",
+        headerTitle: () => (
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+            <Text style={[fonts.h2]}>BUZÓN</Text>
+            <Text style={[fonts.h3]}>(SOLICITANTES)</Text>
+          </View>
+        ),
+
+        headerLeft: () => (
+          <View style={{ marginLeft: 20, marginRight: 10 }}>
+            <Ionicons
+              name={"people-outline"}
+              size={38}
+              color={colors.textColor}
+            />
+          </View>
+        ),
+      }}
+    >
       <MessageStack.Screen
         name="BUZON DE MENSAJES"
         component={MessagesScreen}
       />
-       <MessageStack.Screen
+      <MessageStack.Screen
         name="CHAT CON SOLICITANTE"
         component={MessagesDetailScreen}
         options={({ route }) => ({
@@ -27,4 +58,4 @@ export const MessageNavigator = () => {
   );
 };
 
-export default MessageNavigator
+export default MessageNavigator;
