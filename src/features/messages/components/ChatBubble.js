5f854617aa0912ae181e8b697104e9e2b85757ce
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-03-27 13:25:41 -0500

    mini component

---
+++ src/features/messages/components/ChatBubble.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { useTheme } from "react-native-paper";
+
+const ChatBubble = ({ msg, profile, isUser }) => {
+
+  const { colors, fonts } = useTheme();
+
+  return (
+    <View
+    key={index}
+    style={{
+      flexDirection: "row",
+      alignItems: "flex-end",
+      marginVertical: 5,
+      alignSelf: isUser ? "flex-end" : "flex-start",
+      justifyContent: isUser ? "flex-end" : "flex-start",
+    }}
+  >
+    {!isUser && (
+      <Image
+        source={{ uri: profile?.profileImage }}
+        style={{
+          width: 30,
+          height: 30,
+          borderRadius: 15,
+          marginRight: 8,
+          elevation: 5,
+        }}
+      />
+    )}
+    <View
+      style={{
+        maxWidth: "75%",
+        padding: 10,
+        borderRadius: 10,
+        elevation: 5,
+        backgroundColor: colors.darkerBaseColor,
+      }}
+    >
+      <Text style={[fonts.label2]}>{msg.text}</Text>
+      <Text style={[fonts.label2]}>{msg.timestamp}</Text>
+    </View>
+  </View>
+  );
+};
+
+export default ChatBubble;
