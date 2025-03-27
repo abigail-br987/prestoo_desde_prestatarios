949a5a434ee3821c61ad85ccff9d28a9e006062f
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-03-27 10:46:31 -0500

    add more functionality for messages

--- src/infraestructure/navigation/HomeNavigator.js
+++ src/infraestructure/navigation/HomeNavigator.js
@@ -6,6 +6,9 @@ import { HomeDetailScreen } from "../../features/home/screens/HomeDetailScreen";
 import { Text } from "react-native";
 import { useTheme } from "react-native-paper";
 import { Button1 } from "../../features/home/components/Button1";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Pressable } from "react-native";
 
 const HomeStack = createStackNavigator();
 export const HomeNavigator = () => {
@@ -53,20 +56,69 @@ export const HomeNavigator = () => {
           ),
         }}
       />
-      <HomeStack.Screen
-        name="INFO DEL SOLICITANTE"
 
-        screenOptions={{
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: colors.baseColor,
-            elevation: 10,
-          },
+
+<HomeStack.Screen
+  name="INFO DEL SOLICITANTE"
+  component={HomeDetailScreen}
+  options={({ route, navigation }) => ({
+    headerTitle: () => (
+      <Text style={[fonts.h3, { textTransform: "uppercase" }]}>
+        {route.params.profile.name}
+      </Text>
+    ),
+    headerLeft: () => (
+      <TouchableOpacity
+        style={{ marginLeft: 15 }}
+        onPress={() => navigation.goBack()}
+      >
+        <Ionicons name="arrow-back" size={24} color={colors.textColor} />
+      </TouchableOpacity>
+    ),
+    headerRight: () => (
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginRight: 15,
         }}
+      >
+        <Pressable
+          style={{
+            paddingVertical: 5,
+            paddingHorizontal: 12,
+            backgroundColor: colors.thirdAccent,
+            borderRadius: 5,
+            flexDirection: "row",
+            alignItems: "center",
+            elevation: 5,
+          }}
+          onPress={() =>
+            navigation.navigate("Mensajes", {
+              screen: "CHAT CON SOLICITANTE",
+              params: { profileId: route.params.profile.id, name: route.params.profile.name },
+            })
+          }
+        >
+          <Ionicons
+            name="chatbubble-ellipses"
+            size={15}
+            color={colors.textColor}
+          />
+          <Text style={[fonts.h3, { marginLeft: 5 }]}>Contacto</Text>
+        </Pressable>
+      </View>
+    ),
+  })}
+/>
 
-        
-        component={HomeDetailScreen}
-      />
+
+
+
     </HomeStack.Navigator>
+
+
+
+
   );
 };
