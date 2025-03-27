dce46616f7f4a86a876c247e0ec49c65ef335ea0
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-03-27 10:45:53 -0500

    add icon

---
+++ src/features/home/components/IconButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const IconButton = ({ onPress, iconName, color, style }) => {
+
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[
+        {
+          backgroundColor: color,
+          padding: 2,
+          elevation: 5,
+          borderRadius: 50, 
+          zIndex: 2,
+        },
+        style,
+      ]}
+    >
+      <Ionicons name={iconName} size={24} color={"#053654"}/>
+    </TouchableOpacity>
+  );
+};
+
+export default IconButton;
