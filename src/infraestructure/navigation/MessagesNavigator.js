e148034f765e8537c831fcbe6fcda45f6f97b332
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-03-26 21:40:03 -0500

    update names

--- src/infraestructure/navigation/MessagesNavigator.js
+++ src/infraestructure/navigation/MessagesNavigator.js
@@ -1,23 +1,30 @@
-import {
-  createStackNavigator,
-  TransitionPresets,
-} from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import MessagesScreen from "../../features/messages/screens/MessagesScreen"
+import MessagesDetailScreen from "../../features/messages/screens/MessagesDetailScreen";
 
-import { MessagesDetailScreen } from '../../features/messages/screens/MessagesDetailScreen';
-import { MessagesScreen } from '../../features/messages/screens/MessagesScreen';
-const RestaurantStack = createStackNavigator();
+const MessageStack = createStackNavigator();
 
-export const MessagesNavigator = () => {
+export const MessageNavigator = () => {
+
+  console.log("MessagesScreen:", MessagesScreen);
+
+
   return (
-    <RestaurantStack.Navigator>
-      <RestaurantStack.Screen
-        name="Restaurants"
+    <MessageStack.Navigator screenOptions={{ headerShown: true }}>
+      <MessageStack.Screen
+        name="BUZON DE MENSAJES"
         component={MessagesScreen}
       />
-      <RestaurantStack.Screen
-        name="RestaurantDetail"
+       <MessageStack.Screen
+        name="CHAT CON SOLICITANTE"
         component={MessagesDetailScreen}
+        options={({ route }) => ({
+          title: route.params.name,
+        })}
       />
-    </RestaurantStack.Navigator>
+    </MessageStack.Navigator>
   );
 };
+
+export default MessageNavigator
