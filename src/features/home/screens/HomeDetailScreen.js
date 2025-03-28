ebe6358b7f588b39831fac6b088f1e67b332c920
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-03-27 23:10:16 -0500

    change ui

--- src/features/home/screens/HomeDetailScreen.js
+++ src/features/home/screens/HomeDetailScreen.js
@@ -17,107 +17,68 @@ export const HomeDetailScreen = ({ route, navigation }) => {
       style={{
         flex: 1,
         padding: 20,
-        gap: 25,
         backgroundColor: colors.secondaryLightColor,
       }}
     >
       <View
         style={{
+          backgroundColor: colors.intensePrimaryAccent,
+          borderRadius: 15,
+          padding: 20,
           elevation: 5,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 3 },
+          shadowOpacity: 0.2,
+          shadowRadius: 5,
+          gap: 15,
         }}
       >
-        <View
-          style={{
-            backgroundColor: colors.intensePrimaryAccent,
-            padding: 15,
-            elevation: 5,
-            gap: 10,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-
-            <View style={{flexDirection: "row", alignItems: "center", gap:10,}}>
-            <IconButton
-                onPress={() => toggleSave(profile.id)}
-                iconName={
-                  savedProfiles[profile.id] ? "bookmark" : "bookmark-outline"
-                }
-                style={{}}
-                color={colors.primaryLightColor}
-              />
-
-            <Text style={[fonts.h3]}>{profile.name}</Text>
-
-
-            </View>
-            
-
-          </View>
-
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-              justifyContent: "space-between",
-            }}
-          >
-            <Image
-              source={{ uri: profile.profileImage }}
-              style={{
-                width: 80,
-                height: 80,
-                borderRadius: 50,
-                borderWidth: 2,
-                borderColor: colors.textColor,
-              }}
-            />
-
-            <View style={{ flex: 1 }}>
-              <Text
-                style={[fonts.label2, { flexShrink: 1, textAlign: "justify" }]}
-              >
-                {profile.age} años | {profile.status}{" "}
-              </Text>
-              <Text style={[fonts.label2]}>{profile.shortDescription}</Text>
-            </View>
-          </View>
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+          <IconButton
+            onPress={() => toggleSave(profile.id)}
+            iconName={
+              savedProfiles[profile.id] ? "bookmark" : "bookmark-outline"
+            }
+            color={colors.primaryLightColor}
+          />
+          <Text style={[fonts.h2, { flexShrink: 1 }]}>{profile.name}</Text>
         </View>
 
-        <View
-          style={{ padding: 15, backgroundColor: colors.baseColor, gap: 10 }}
-        >
-          <Text style={[fonts.subheading]}>
-            TASA DE INTERÉS PREFERIDA: {profile.interestRate}{" "}
-          </Text>
-          <Text style={[fonts.subheading]}>
-            TIEMPO DE DEVOLUCIÓN: {profile.repaymentTime}{" "}
-          </Text>
+        <Image
+          source={{ uri: profile.profileImage }}
+          style={{
+            width: "100%",
+            height: 350,
+            borderRadius: 20,
+            borderWidth: 2,
+            borderColor: colors.textColor,
+          }}
+        />
 
-
+        <View>
+          <Text style={[fonts.h3, { textAlign: "justify" }]}>
+            {profile.age} años | {profile.status}
+          </Text>
+          <Text style={[fonts.label2]}>{profile.shortDescription}</Text>
         </View>
       </View>
 
+      <View
+        style={{
+          backgroundColor: colors.baseColor,
+          padding: 15,
+          borderRadius: 15,
+          marginTop: 15,
+          gap: 10,
+        }}
+      >
+        <Text style={[fonts.subheading]}>
+          TASA DE INTERÉS PREFERIDA: {profile.interestRate}
+        </Text>
+        <Text style={[fonts.subheading]}>
+          TIEMPO DE DEVOLUCIÓN: {profile.repaymentTime}
+        </Text>
+      </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  headerText: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  bodyText: {
-    fontSize: 16,
-  },
-  button: {
-    marginLeft: 10,
-    borderWidth: 2,
-  },
-});
